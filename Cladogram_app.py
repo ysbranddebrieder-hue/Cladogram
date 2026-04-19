dd7646567b1f96a3347e0d4dc83f6eb1120a7438
ysbranddebrieder-hue <ysbranddebrieder@gmail.com>
2026-04-19 15:52:04 +0200

    Update Cladogram_app.py

--- Cladogram_app.py
+++ Cladogram_app.py
@@ -8,15 +8,15 @@ from io import BytesIO
 st.set_page_config(layout="wide")
 st.title("Diagonaal Vertebrata Cladogram")
 
-# 1. Data
+# 1. Data voor Vertebrata
 data = {
-    'Wervels':  [1, 1, 1, 1, 1, 0],
-    'Kaken':    [1, 1, 1, 1, 0, 0],
-    'Poten':    [1, 1, 1, 0, 0, 0],
-    'Amnion':   [1, 1, 0, 0, 0, 0],
-    'Haar/Melk':[1, 0, 0, 0, 0, 0]
+    'Wervels': ,
+    'Kaken':   ,
+    'Poten':   ,
+    'Amnion':  ,
+    'Haar/Melk':
 }
-index = ['Mens', 'Hagedis', 'Kikker', 'Haai', 'Prik', 'Vlieg']
+index = ['Mens', 'Hond', 'Krokodil', 'Kikker', 'Haai', 'Prik']
 df = pd.DataFrame(data, index=index)
 
 st.subheader("1. Karaktermatrix")
@@ -24,47 +24,53 @@ edited_df = st.data_editor(df, use_container_width=True)
 
 if st.button("🔄 Teken Diagonaal Cladogram"):
     try:
-        # Sorteer op complexiteit
+        # Sorteer op complexiteit (aantal 1-en)
         sorted_df = edited_df.iloc[np.argsort(edited_df.sum(axis=1))]
         Z = linkage(sorted_df, method='weighted', optimal_ordering=True)
         
         fig, ax = plt.subplots(figsize=(10, 8))
         
-        # 'link_color_func' gebruiken we om alle lijnen zwart te maken
-        # 'orientation=top' is de basis, maar we passen de tekenstijl aan
-        ddata = dendrogram(
-            Z, 
-            labels=sorted_df.index, 
-            orientation='top', 
-            ax=ax,
-            no_plot=True # We vangen de data op om zelf schuin te tekenen
-        )
+        # We vangen de data op zonder direct te plotten
+        ddata = dendrogram(Z, labels=sorted_df.index, no_plot=True)
 
-        # Teken handmatig de schuine lijnen (Slanted style)
+        # Teken handmatig de schuine (diagonale) lijnen
         for icoord, dcoord in zip(ddata['icoord'], ddata['dcoord']):
-            x = [icoord[0], (icoord[1]+icoord[2])/2, icoord[3]]
+            # Maak een V-vorm van de takken
+            x = [icoord[0], (icoord[0] + icoord[3]) / 2, icoord[3]]
             y = [dcoord[0], dcoord[1], dcoord[3]]
             ax.plot(x, y, color='black', lw=2)
 
-        # Voeg labels voor eigenschappen toe op de schuine ruggengraat
+        # Plaats eigenschappen op de knooppunten
         kenmerken = sorted_df.columns
         for i, (icoord, dcoord) in enumerate(zip(ddata['icoord'], ddata['dcoord'])):
             if i < len(kenmerken):
-                x_mid = (icoord[1] + icoord[2]) / 2
-                y_mid = dcoord[1]
-                ax.plot(x_mid, y_mid, 'ro', markersize=8)
-                ax.text(x_mid + 2, y_mid, f" {kenmerken[i]}", color='red', fontweight='bold')
+                x_node = (icoord[0] + icoord[3]) / 2
+                y_node = dcoord[1]
+                ax.plot(x_node, y_node, 'ro', markersize=8)
+                ax.text(x_node + 2, y_node, f" {kenmerken[i]}", 
+                        color='red', fontweight='bold', va='center')
 
-        # Opmaak
-        ax.set_xticks(ddata['icoord'])
+        # FIX voor de Error: Gebruik de individuele blad-posities voor de X-as
+        leaf_positions = np.array([ (icoord[0] if dcoord[0] == 0 else icoord[3]) 
+                                   for icoord, dcoord in zip(ddata['icoord'], ddata['dcoord']) 
+                                   if dcoord[0] == 0 or dcoord[3] == 0 ])
+        
+        # Zorg dat we alleen unieke posities voor de labels gebruiken
+        unique_pos = np.unique(np.array(ddata['icoord']).flatten())
+        ax.set_xticks(np.arange(5, len(sorted_df) * 10 + 5, 10))
         ax.set_xticklabels(ddata['ivl'], rotation=45, ha='right')
-        ax.axis('off')
+
+        ax.spines['top'].set_visible(False)
+        ax.spines['right'].set_visible(False)
+        ax.spines['left'].set_visible(False)
+        ax.set_yticks([]) # Verwijder y-as voor een cleaner uiterlijk
+        
         st.pyplot(fig)
         
-        # Download
+        # Downloadoptie
         buf = BytesIO()
         fig.savefig(buf, format="png", bbox_inches='tight')
-        st.download_button("💾 Download Diagonaal Cladogram", buf.getvalue(), "slanted_cladogram.png")
+        st.download_button("💾 Download Diagonaal Cladogram", buf.getvalue(), "diagonaal_cladogram.png")
         
     except Exception as e:
         st.error(f"Fout: {e}")
